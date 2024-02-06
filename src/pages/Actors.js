fee60ef12d012519b491cb8f9af5f701c769614e
Adrian Suarez <asuarez9382@gmail.com>
2024-02-06 12:28:27 -0800

    complete lab

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,36 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { act } from "react-dom/test-utils";
 
 function Actors() {
+
+  const [actors, setActors] = useState([])
+
+  useEffect(()=> {
+    fetch("http://localhost:4000/actors")
+      .then(res => res.json())
+      .then(data => setActors(data))
+  }, [])
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <h1>Actors Page</h1>
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        {
+          actors.map(actor => (
+            <article key={actor.id}>
+              <h2>{actor.name}</h2>
+              <ul>
+              {actor.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+              </ul>
+            </article>
+          ))
+        }
       </main>
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,35 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(()=> {
+    fetch("http://localhost:4000/movies")
+    .then(res => res.json())
+    .then(data => setMovies(data))
+  }, [])
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <h1>Home</h1>
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        {
+          movies.map(movie => (
+          <MovieCard
+            title={movie.title}
+            time={movie.time}
+            genres={movie.genres}
+            id={movie.id}
+            key={movie.id}
+          />)
+          )
+        }
       </main>
     </>
   );
